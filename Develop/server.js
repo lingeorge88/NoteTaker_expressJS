dae6a061094ba799bf7b481ca5ed0772848c5f56
lingeorge88 <lingeorge04@gmail.com>
2023-03-15 18:01:27 -0700

    updated server.js javascript

---
+++ Develop/server.js
@@ -0,0 +1,20 @@
+const express = require('express');
+const path = require('path');
+const PORT = 3001;
+const app = express();
+
+app.use(express.static('public'));
+
+// GET Route for homepage
+app.get("*", (req,res) =>
+res.sendFile(path.join(__dirname, '/public/index.html'))
+);
+
+// GET route for notes page
+app.get("/notes", (req, res) =>
+res.sendFile(path.join(__dirname, '/public/notes.html'))
+);
+
+app.listen(PORT, () =>
+  console.log(`App listening at http://localhost:${PORT} 🚀`)
+);
