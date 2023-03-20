00d072f29ef356942accbb745ad3036ccfc9fff4
lingeorge88 <lingeorge04@gmail.com>
2023-03-19 18:27:05 -0700

    added GET route for /api/notes

--- Develop/server.js
+++ Develop/server.js
@@ -1,8 +1,10 @@
 const express = require('express');
 const path = require('path');
-
+const { readFromFile, readAndAppend } = require('./public/assets/js/fsUtils');
+const { v4: uuidv4 } = require('uuid');
 const PORT = 3001;
 const app = express();
+
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static('public'));
@@ -15,7 +17,14 @@ res.sendFile(path.join(__dirname, '/public/index.html'))
 app.get("/notes", (req, res) =>
 res.sendFile(path.join(__dirname, '/public/notes.html'))
 );
-
+// GET route for api/notes that returns saved notes as JSON
+app.get('/api/notes', (req, res) => {
+  readFromFile('./db/db.json').then((data) => res.json(JSON.parse(data)));
+});
+// GET route for routes that don't exist
+app.get('*', (req, res) =>
+  res.sendFile(path.join(__dirname, '/public/index.html')
+));
 app.listen(PORT, () =>
   console.log(`App listening at http://localhost:${PORT} 🚀`)
 );
