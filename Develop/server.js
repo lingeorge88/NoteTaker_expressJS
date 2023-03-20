ae50be244a2356a5ad63637af7106aef314431ff
lingeorge88 <lingeorge04@gmail.com>
2023-03-19 18:32:29 -0700

    added POST route for api/notes

--- Develop/server.js
+++ Develop/server.js
@@ -21,6 +21,25 @@ res.sendFile(path.join(__dirname, '/public/notes.html'))
 app.get('/api/notes', (req, res) => {
   readFromFile('./db/db.json').then((data) => res.json(JSON.parse(data)));
 });
+
+app.post('/api/notes', (req, res) => {
+  console.log(req.body);
+
+  const { title, text } = req.body;
+
+  if (req.body) {
+    const newNote = {
+      title,
+      text,
+      note_id: uuidv4(),
+    };
+
+    readAndAppend(newNote, './db/db.json');
+    res.json(`Note added successfully 🚀`);
+  } else {
+    res.error('Error in adding new note');
+  }
+});
 // GET route for routes that don't exist
 app.get('*', (req, res) =>
   res.sendFile(path.join(__dirname, '/public/index.html')
