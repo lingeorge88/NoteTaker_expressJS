48c3496a34fcb20894ff58ad7467c1511e203f6b
lingeorge88 <lingeorge04@gmail.com>
2023-03-19 17:35:27 -0700

    updated GET routes for homepage and notes page

--- Develop/server.js
+++ Develop/server.js
@@ -1,10 +1,10 @@
 const express = require('express');
 const path = require('path');
+
 const PORT = 3001;
 const app = express();
 
 app.use(express.static('public'));
-
 // GET Route for homepage
 app.get("*", (req,res) =>
 res.sendFile(path.join(__dirname, '/public/index.html'))
